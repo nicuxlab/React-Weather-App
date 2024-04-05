d03e6655d3777284a072e48ae1a3b4af835b1350
nicuxlab <sedjroayimande@gmail.com>
2024-04-05 12:43:07 +0200

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Codé par
+      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
+        > AYIMANDE Nicaise</a>, <a href="https://github.com/shemmee/React-Weather-App">Lien sur github.</a> 
+    </p>
+  </div>
+  );
+}
---
+++ src/components/SearchEngine.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SearchEngine({ query, setQuery, search }) {
+  return (
+    <div className="SearchEngine">
+      
+      <input
+        type="text"
+        className="city-search"
+        placeholder="Entrer le nom de la ville"
+        name="query"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      <button><i className="fas fa-search" style={{ fontSize: "18px" }}></i></button>
+    </div>
+  );
+}
+
+export default SearchEngine;
